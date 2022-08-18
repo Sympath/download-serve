26e78c671ee150c50a4c7fbb9c1ecbcc02d1e950
Wzyan <3101885298@qq.com>
2022-08-18 13:37:25 +0800

    支持指定课程上传

--- routes/index.js
+++ routes/index.js
@@ -3,25 +3,33 @@ const path = require('path')
 const utils = require('../utils/index')
 let getCloneAllShRepoCmd = (name) => `git clone git@github.com:Sympath/download-sh.git ${path.resolve(__dirname, '../all-kkb/' + name)}`
 let getStartDownCmd = (name) => `cd ${path.resolve(__dirname, '../all-kkb/' + name)} && sh all.sh  1>all.log 2>all_err.log`
+let getFormatConfigCmd = (name, cookie, courseIds = []) => `
+cat >> ${path.resolve(__dirname, '../all-kkb/' + name)}/config << EOF
+name=${name}
+cookie=${cookie}
+courseIds=${courseIds}
+EOF
+`
 let getFormatConfigNameCmd = (name, cookie) => `echo ${name} > ${path.resolve(__dirname, '../all-kkb/' + name)}/name.txt`
 let getFormatConfigCookieCmd = (name, cookie) => `echo ${cookie} > ${path.resolve(__dirname, '../all-kkb/' + name)}/cookie.txt `
 router.post('/start', async (ctx, next) => {
   debugger
-  let { cookie, name } = ctx.request.body
+  // courseIds 用于指定要下载那些课程；如果为空数组则全部下载
+  let { cookie, name, courseIds } = ctx.request.body
   console.log(cookie, name);
   if (typeof cookie === 'object') {
     cookie = JSON.stringify(cookie)
   }
   // cookie = decodeURIComponent(cookie)
-  debugger
   try {
     // 创建空间
     await utils.checkPath(path.resolve(__dirname, '../all-kkb'))
     // 下载脚本仓库
     let cloneAllShRepoCmd = getCloneAllShRepoCmd(name);
     // 更换配置
-    let formatConfigNameCmd = getFormatConfigNameCmd(name, cookie);
-    let formatConfigCookieCmd = getFormatConfigCookieCmd(name, cookie);
+    let formatConfigCmd = getFormatConfigCmd(name, cookie, courseIds);
+    // let formatConfigNameCmd = getFormatConfigNameCmd(name, cookie);
+    // let formatConfigCookieCmd = getFormatConfigCookieCmd(name, cookie);
     // 执行启动下载脚本
     let startDownCmd = getStartDownCmd(name)
     let cmds = [
@@ -30,16 +38,17 @@ router.post('/start', async (ctx, next) => {
       `cd ${path.resolve(__dirname, '../all-kkb')}`,
       cloneAllShRepoCmd,
       formatConfigNameCmd,
+      formatConfigCmd,
       // formatConfigCookieCmd,
-      // startDownCmd
+      startDownCmd
     ]
     process.nextTick(
       async () => {
         try {
           await utils.doShellAllCmd(cmds)
           console.log('下载开始');
-          await utils.writeFileRecursive(`${path.resolve(__dirname, '../all-kkb/' + name)}/cookie.txt`, cookie)
-          utils.doShellCmd(startDownCmd)
+          // await utils.writeFileRecursive(`${path.resolve(__dirname, '../all-kkb/' + name)}/cookie.txt`, cookie)
+          // utils.doShellCmd(startDownCmd)
         } catch (error) {
           console.log(`执行失败${error}`);
           ctx.body = `下载失败，原因: ${error}`
