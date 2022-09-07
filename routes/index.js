7cc780c553c9726578798244b0f15b4f4a83a67c
Wzyan <3101885298@qq.com>
2022-09-07 10:39:27 +0800

    feat: 支持小鹅通配置文件生成

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const path = require('path')
 const utils = require('../utils/index')
 let getCloneAllShRepoCmd = (currentShRepoPath) => `git clone git@github.com:Sympath/download-sh.git ${currentShRepoPath}`
 let getStartDownCmd = (currentShRepoPath) => `cd ${currentShRepoPath} && npm run all`
+let getXiaoETDownCmd = (currentShRepoPath) => `cd ${currentShRepoPath} && npm run xiaoet-all`
 let getRunDownCmd = (currentShRepoPath) => `cd ${currentShRepoPath} && npm run config-retry`
 // 下载单个视频时写入本地文件配置
 let getSingleFormatConfigCmd = (m3u8Url, courseName, bypyFullDir, bypyDir) => {
@@ -175,6 +176,59 @@ router.post('/down-single', async (ctx, next) => {
     ctx.body = '执行成功，正在重传'
   } catch (error) {
     ctx.body = `执行失败，原因: ${error}`
+  }
+})
+// 小鹅通配置导出
+router.post('/xiaoet-start', async (ctx, next) => {
+  // courseIds 用于指定要下载那些课程；如果为空数组则全部下载
+  let { cookie, name, courseIds, donwType = 'down-all' } = ctx.request.body
+  console.log(cookie, name);
+  let currentRepoPath = path.resolve(__dirname, '../all-kkb/' + name + '/repo')
+  let currentShRepoPath = path.resolve(__dirname, '../all-kkb/' + name)
+  debugger
+  if (typeof cookie === 'object') {
+    cookie = JSON.stringify(cookie)
+  }
+  if (typeof courseIds === 'undefined') {
+    courseIds = '*'
+  }
+  // cookie = decodeURIComponent(cookie)
+  try {
+    // 创建空间
+    await utils.checkPath(path.resolve(__dirname, '../all-kkb'))
+    debugger
+    // 下载脚本仓库
+    let cloneAllShRepoCmd = getCloneAllShRepoCmd(currentShRepoPath);
+    // 更换配置
+    let formatConfigCmd = getFormatConfigCmd(currentShRepoPath, name, cookie, courseIds, donwType);
+    // let formatConfigNameCmd = getFormatConfigNameCmd(name, cookie);
+    // let formatConfigCookieCmd = getFormatConfigCookieCmd(name, cookie);
+    // 执行启动下载脚本
+    let startDownCmd = getXiaoETDownCmd(currentShRepoPath)
+    let cmds = [
+      // `rm -rf ${currentShRepoPath}`,
+      cloneAllShRepoCmd,
+      // formatConfigNameCmd,
+      formatConfigCmd,
+      // formatConfigCookieCmd,
+      startDownCmd
+    ]
+    process.nextTick(
+      async () => {
+        try {
+          await utils.doShellAllCmd(cmds)
+          console.log('下载开始');
+          // await utils.writeFileRecursive(`${path.resolve(__dirname, '../all-kkb/' + name)}/cookie.txt`, cookie)
+          // utils.doShellCmd(startDownCmd)
+        } catch (error) {
+          console.log(`执行失败${JSON.stringify(error)}`);
+          ctx.body = `下载失败，原因: ${JSON.stringify(error)}`
+        }
+      }
+    )
+    ctx.body = '执行成功，正在下载'
+  } catch (error) {
+    ctx.body = `下载失败，原因: ${error}`
   }
 })
 module.exports = router
